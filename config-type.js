c804c1d2d0ee9b70427b90a80f40ed008c6e69fb
fhenri42 <fhenri@student.42.fr>
2016-10-03 09:35:10 +0200

    [UPD]

---
+++ config-type.js
@@ -0,0 +1,23 @@
+let config = {}
+config.recast = {}
+config.recast.request_token = 'TOKEN'
+config.recast.language = 'fr'
+config.kik = {}
+config.kik.username = 'NAME_KIK'
+config.kik.apiKey = 'API_KEY'
+config.kik.baseUrl = 'NGROK_URL'
+config.nasa = {}
+config.nasa.token = 'TOKEN_NASA'
+
+module.exports = config
+
+
+/*
+  -Name of the camera
+  -FHAZ
+  -NAVCAM
+  -MAST
+  -CHEMCAM
+  -MAHLI
+  -MARDI
+*/
